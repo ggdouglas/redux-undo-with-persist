93e866a95bd61c5bd3415bdc2fe1ce3b94311b96
Greg Douglas <greg@storyblocks.com>
2020-06-09 16:25:43 -0400

    ooh, i may have found the answer!

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 import { persistStore, persistReducer } from 'redux-persist';
+import { ActionCreators as UndoActionCreators } from 'redux-undo';
 import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
 
@@ -13,4 +14,6 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = createStore(persistedReducer, {}, devToolsEnhancer());
 
-export const persistor = persistStore(store);
+export const persistor = persistStore(store, null, () => {
+  store.dispatch(UndoActionCreators.clearHistory());
+});
